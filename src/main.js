9cd06d738a6b9c2debca85b535b2923da41a2864
BrayanGuerreroXD <bg7047653@gmail.com>
2023-01-06 08:54:57 -0500

    Add new li logout in index.html and import auth and logout file

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js"
+import { auth } from "./app/firebase.js"
+
 import './app/signupForm.js'
+import './app/logout.js'
+
+onAuthStateChanged(auth, async (user) => {
+    console.log(user);
+})
