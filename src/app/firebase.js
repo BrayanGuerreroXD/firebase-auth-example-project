a6b720fb045d2555b5387c406cf18082e9e3ea85
BrayanGuerreroXD <bg7047653@gmail.com>
2023-01-06 15:11:25 -0500

    Add firebase store

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -4,6 +4,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebas
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js"
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -18,3 +19,6 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
+
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore(app);
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js"
-import { auth } from "./app/firebase.js"
+import { getDocs, collection } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js"
+import { auth, db } from "./app/firebase.js"
 import { loginCheck } from './app/loginCheck.js'
+import {setupPosts} from './app/postList.js'
 
 import './app/signupForm.js'
 import './app/logout.js'
@@ -11,7 +13,8 @@ import './app/githubLogin.js'
 
 onAuthStateChanged(auth, async (user) => {
     if (user) {
-        loginCheck(user)
+        const querySnapshot = await getDocs(collection(db, 'posts'))
+        setupPosts(querySnapshot.docs)
     } else {
         loginCheck()
     }
