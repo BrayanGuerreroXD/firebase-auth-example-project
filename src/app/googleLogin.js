649b21dfb2a0e10b8b59952b0a5a1c1b0885af12
BrayanGuerreroXD <bg7047653@gmail.com>
2023-01-06 10:50:49 -0500

    Add login with google authentication

---
+++ src/app/googleLogin.js
@@ -0,0 +1,20 @@
+import { GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js"
+import { auth } from "./firebase.js"
+import { showMessage } from "./showMessage.js"
+
+const googleButton = document.querySelector('#google-login')
+
+googleButton.addEventListener('click', async () => {
+    const provider = new GoogleAuthProvider()
+    try {
+        const credentials = await signInWithPopup(auth, provider)
+        console.log(credentials);
+
+        const modal = bootstrap.Modal.getInstance(document.querySelector('#signinModal'))
+        modal.hide()
+
+        showMessage(`Welcome ${credentials.user.displayName}`)
+    } catch (error) {
+        console.log(error);
+    }
+})
