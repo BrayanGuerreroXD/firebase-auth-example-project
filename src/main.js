2086d3682fa5fcd330dabe8d9ff923ee9b3d26ab
BrayanGuerreroXD <bg7047653@gmail.com>
2023-01-06 09:10:26 -0500

    Add loginCheck function to main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js"
 import { auth } from "./app/firebase.js"
+import { loginCheck } from './app/loginCheck.js'
 
 import './app/signupForm.js'
 import './app/logout.js'
 
 onAuthStateChanged(auth, async (user) => {
-    console.log(user);
+    if (user) {
+        loginCheck(user)
+    } else {
+        loginCheck()
+    }
 })
